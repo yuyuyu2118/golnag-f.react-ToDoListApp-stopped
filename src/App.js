00b5a6b7242ca910153b045dae28158ce48c83bb
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-06-15 10:11:06 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {Container} from "semantic-ui-react";
+import ToDoList from "./To-Do-List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <ToDoList></ToDoList>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/To-Do-List.js
@@ -0,0 +1,34 @@
+import React,{Component} from "react";
+import axios from "axios";
+import {Card,Header,Form,Input,Icon} from "semantic-ui-react";
+
+let endpoint = "http://localhost:60180";
+
+class ToDoList extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            task:"",
+            items:[],
+        };
+    }
+
+    componentDidMount(){
+        this.getTask();
+    }
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <Header className="header" as="h2" color="yellow">
+                        To Do List
+                    </Header>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default ToDoList;
